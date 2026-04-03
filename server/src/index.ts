b36f3a3c618b27a77c42a69d96d34784598f2a4b
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-04-03 18:45:52 -0400

    feat: using express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello, TypeScript!')
+import express from 'express'
+
+const app = express()
+
+app.get('/', (_, res) => {
+  res.send('Hello World!')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
