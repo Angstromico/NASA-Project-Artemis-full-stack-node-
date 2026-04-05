20669d6bf4705e87e22f8f35cf0a0be377f1ec16
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-04-05 11:13:30 -0400

    refactor: planets data for CSV Kepler file

--- server/src/models/planets.model.ts
+++ server/src/models/planets.model.ts
@@ -1,12 +1,57 @@
-const planets = [
-  {
-    name: 'Earth',
-    color: 'blue',
-  },
-  {
-    name: 'Mars',
-    color: 'red',
-  },
-]
+import { parse } from 'csv-parse'
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-export default planets
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+interface KeplerPlanet {
+  koi_disposition: string
+  koi_insol: string
+  koi_prad: string
+  kepler_name?: string
+}
+
+export interface HabitablePlanet {
+  name: string
+  color: string
+}
+
+function isHabitablePlanet(planet: KeplerPlanet): boolean {
+  return (
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    Number(planet['koi_insol']) > 0.36 &&
+    Number(planet['koi_insol']) < 1.11 &&
+    Number(planet['koi_prad']) < 1.6
+  )
+}
+
+const loadPlanetsData = (): Promise<KeplerPlanet[]> => {
+  return new Promise((resolve, reject) => {
+    const results: KeplerPlanet[] = []
+
+    fs.createReadStream(path.join(__dirname, '../data/kepler_data.csv'))
+      .pipe(
+        parse({
+          comment: '#',
+          columns: true,
+        })
+      )
+      .on('data', (data: KeplerPlanet) => {
+        if (isHabitablePlanet(data)) {
+          results.push(data)
+        }
+      })
+      .on('error', (err) => {
+        console.error(err)
+        reject(err)
+      })
+      .on('end', () => {
+        console.log(`${results.length} habitable planets found!`)
+        resolve(results)
+      })
+  })
+}
+
+export default loadPlanetsData
--- server/src/routes/planets/planets.controller.ts
+++ server/src/routes/planets/planets.controller.ts
@@ -2,8 +2,10 @@ import type { Request, Response } from 'express'
 import { OK } from '../../utils/http-codes.js'
 import planets from '../../models/planets.model.js'
 
-function getAllPlanets(_: Request, res: Response): Response {
-  return res.status(OK).json(planets)
+async function getAllPlanets(_: Request, res: Response): Promise<Response> {
+  const planetData = await planets()
+
+  return res.status(OK).json(planetData)
 }
 
 export { getAllPlanets }
