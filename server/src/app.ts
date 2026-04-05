71db155bf21aebc9919ab052f32f369f9fefe042
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-04-05 10:00:46 -0400

    refactor: cors config

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 
+const whitelist = [process.env.CORS_ORIGIN || 'http://localhost:5173']
+
 const app = express()
-app.use(cors())
+app.use(
+  cors({
+    origin: whitelist,
+  })
+)
 app.use(express.json())
 
 export default app
