5fe095aa9c2517753d29a864eea175c5d6e8103a
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-04-05 13:23:07 -0400

    fix: right key name in selectorBody

--- client/src/pages/Launch.js
+++ client/src/pages/Launch.js
@@ -5,7 +5,7 @@ import Clickable from '../components/Clickable'
 const Launch = (props) => {
   const selectorBody = useMemo(() => {
     return props.planets?.map((planet, index) => (
-      <option value={planet.name} key={planet.kepler_name || index}>
+      <option value={planet.kepler_name} key={planet.kepler_name || index}>
         {planet.kepler_name}
       </option>
     ))
