f78844168a74db975e784c125110b21526023be1
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-04-05 10:04:44 -0400

    fix: unique key in selectorBody

--- client/src/pages/Launch.js
+++ client/src/pages/Launch.js
@@ -4,8 +4,8 @@ import Clickable from "../components/Clickable";
 
 const Launch = props => {
   const selectorBody = useMemo(() => {
-    return props.planets?.map(planet => 
-      <option value={planet.kepler_name} key={planet.kepler_name}>{planet.kepler_name}</option>
+    return props.planets?.map((planet, index) => 
+      <option value={planet.name} key={planet.name || index}>{planet.name}</option>
     );
   }, [props.planets]);
 
